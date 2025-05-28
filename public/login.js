e6b5130b4369fd115f277637ce134350cff54e79
Notluf22 <fultonsheen2020@gmail.com>
2025-05-28 19:20:41 +0530

    main

--- public/login.js
+++ public/login.js
@@ -1,6 +1,6 @@
 // firebaseConfig.js
 import { initializeApp } from "https://www.gstatic.com/firebasejs/11.8.1/firebase-app.js";
-import { getAuth, createUserWithEmailAndPassword } from "https://www.gstatic.com/firebasejs/11.8.1/firebase-auth.js";
+import { getAuth, signInWithEmailAndPassword } from "https://www.gstatic.com/firebasejs/11.8.1/firebase-auth.js";
 import { getFirestore } from "https://www.gstatic.com/firebasejs/11.8.1/firebase-firestore.js";
 
 const firebaseConfig = {
@@ -23,12 +23,12 @@ submit.addEventListener("click", function (event) {
   const email = document.getElementById('email').value;
   const password = document.getElementById('password').value;
 
-  createUserWithEmailAndPassword(auth, email, password)
+  signInWithEmailAndPassword(auth, email, password)
     .then((userCredential) => {
       // Signed up 
       const user = userCredential.user;
-      alert("Creating Account...")
-      window.location.href = "login.html";
+      alert("Logging in...")
+      window.location.href = "dashboard.html";
       // ...
     })
     .catch((error) => {
