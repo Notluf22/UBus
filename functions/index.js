4c41f179a04369c2d1bac68b82c95bdc3b9372b4
Notluf22 <fultonsheen2020@gmail.com>
2025-06-02 10:46:06 +0530

    main

--- functions/index.js
+++ functions/index.js
@@ -12,7 +12,7 @@ const logger = require("firebase-functions/logger");
 
 // Create and deploy your first functions
 // https://firebase.google.com/docs/functions/get-started
-
+ exports.hello
 // exports.helloWorld = onRequest((request, response) => {
 //   logger.info("Hello logs!", {structuredData: true});
 //   response.send("Hello from Firebase!");
